73804f5b2fd744633544a543de9e7d1161e883f8
Mateo Torres Arredondo <mateotorres2409@gmail.com>
2025-08-31 18:05:12 -0500

    add method generate uuid

--- demo-nodejs/app/main.js
+++ demo-nodejs/app/main.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 const port = 3000;
 
-app.get('/hello', (req, res) => {
-  res.send('hello world');
+
+app.get("/uuid", (req, res) => {
+  const id = uuidv4();
+  res.json({ uuid: id });
 });
 
 app.listen(port, () => {
